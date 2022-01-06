e7db46ff9a386601a8d1df2ff2ac0a13adf738c8
Gaël Achard de la Vente <gael.achard_de_la_vente@etudiant.univ-lr.fr>
2022-01-06 17:46:32 +0100

    style(app-theme): add light and dark theme, add activeTheme

---
+++ lib/presentation/dimension.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class AppPadding {
+  static const padding1 = EdgeInsets.all(4.0);
+  static const padding2 = EdgeInsets.all(8.0);
+  static const padding3 = EdgeInsets.all(12.0);
+  static const padding4 = EdgeInsets.all(16.0);
+  static const padding5 = EdgeInsets.all(20.0);
+}
+
+abstract class AppRadius {
+  static BorderRadius radius1 = BorderRadius.circular(16.0);
+  static BorderRadius radius2 = BorderRadius.circular(24.0);
+  static BorderRadius radius3 = BorderRadius.circular(40.0);
+}
