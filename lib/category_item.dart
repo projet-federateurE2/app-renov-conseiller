94979fbc98d7d189f7f261a92f08e74be6059707
Gaël Achard de la Vente <gael.achard_de_la_vente@etudiant.univ-lr.fr>
2022-01-13 11:13:07 +0100

    feat/style(category_item): add selected attribute, border radius

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -4,28 +4,53 @@ import 'package:git/presentation/app_icon.dart';
 import 'package:git/presentation/app_theme.dart';
 import 'package:git/presentation/dimension.dart';
 
-class CategoryItem extends StatelessWidget {
+class CategoryItem extends StatefulWidget {
   final String title;
   final String subtitle;
   final Function()? onPressed;
   final IconData icon;
+  final bool selected;
 
   CategoryItem(
       {required this.title,
       required this.subtitle,
       this.onPressed,
-      required this.icon});
+      required this.icon,
+      required this.selected});
+
+  @override
+  State<CategoryItem> createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> {
+  bool _selected = false;
+
+  @override
+  void initState() {
+    _selected = widget.selected;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onPressed,
+      onTap: () {
+        setState(() {
+          _selected = !_selected;
+        });
+        widget.onPressed;
+      },
       child: Container(
-        width: 500,
+        width: 400,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: AppRadius.radius3,
+          color: _selected ? AppColors.lightPrimaryColorLight : AppColors.white,
+        ),
         padding: AppPadding.padding5,
         child: Row(
           children: [
-            AppIcon(icon: icon),
+            AppIcon(icon: widget.icon),
             Expanded(
               child: Padding(
                 padding: AppPadding.leftPadding5,
@@ -33,14 +58,14 @@ class CategoryItem extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      title,
+                      widget.title,
                       style: activeTheme.textTheme.headline4,
                     ),
                     const SizedBox(
                       height: 4,
                     ),
                     Text(
-                      subtitle,
+                      widget.subtitle,
                       style: activeTheme.textTheme.subtitle1,
                     ),
                   ],
