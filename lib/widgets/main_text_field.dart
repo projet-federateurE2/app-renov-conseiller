a0c288336a230296bc9b6fac514a9be219c46a4d
ErwannBlot <erwann.blot@hotmail.fr>
2022-02-03 10:57:29 +0100

    feat : add textField widget

---
+++ lib/widgets/main_text_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:git/themes/app_theme.dart';
+import 'package:git/themes/dimension.dart';
+
+class MainTextField extends StatelessWidget {
+  final String hint;
+  final bool hasIcon;
+  final String label;
+
+  MainTextField(this.hint, this.hasIcon,this.label, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 650,
+      height: 64,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(20),
+        color: AppColors.lightPrimaryColorLight,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: TextFormField(
+              initialValue: label,
+              style: lightTheme.headline6,
+              decoration: InputDecoration.collapsed(
+                hintText: hint,
+              ),
+            ),
+          ),
+          Visibility(
+            visible: hasIcon,
+            child: const Icon(
+              Icons.close,
+            ),
+          ),
+          const SizedBox(
+            width: 14,
+          ),
+        ],
+      ),
+    );
+  }
+}
