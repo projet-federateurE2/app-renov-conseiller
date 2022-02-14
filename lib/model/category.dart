30f397a25a381155160297012a7d56ff3527aae5
Théo Guillet <theoguillet.etu@gmail.com>
2022-02-14 12:24:19 +0100

    fix(Category) : Add Category constructor fix(Step) : Add Step constructor

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Category {
-  late String name;
-  late String iconPath;
-  late String pitch;
-  late String desc;
-  late List<String> tips;
-  late List<Step> steps;
+  String name;
+  String iconPath;
+  String pitch;
+  String desc;
+  List<String> tips;
+  List<Step> steps;
+
+  Category(
+      this.name, this.iconPath, this.pitch, this.desc, this.tips, this.steps);
 }
--- lib/model/step.dart
+++ lib/model/step.dart
@@ -2,4 +2,6 @@ class Step {
   late String name;
   late List<String> explanations;
   late List<String> subSteps;
+
+  Step(this.name, this.explanations, this.subSteps);
 }
